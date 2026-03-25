341894a1102b59b1a8163f6b8e707fedb6df1d18
Viriathus1 <49337323+Viriathus1@users.noreply.github.com>
2026-03-25 15:00:42 +1100

    added main screen ui

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"image/color"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
+var alphabet = [26]string{
+	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+	"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+}
+
 func NewInputScreen(moveToMainScreen func()) *fyne.Container {
 	title := widget.NewLabel("Simple Substitution Cipher")
+	title.Alignment = fyne.TextAlignCenter
 
 	input := widget.NewEntry()
 	input.SetPlaceHolder("Enter ciphertext...")
@@ -19,14 +28,34 @@ func NewInputScreen(moveToMainScreen func()) *fyne.Container {
 	return container.NewBorder(title, saveButton, nil, nil, input)
 }
 
+func NewMainScreen() *fyne.Container {
+	title := widget.NewLabel("Simple Substitution Cipher")
+	title.Alignment = fyne.TextAlignCenter
+
+	alphabetVStack := container.NewVBox()
+	for _, letter := range alphabet {
+		alphabetVStack.Add(container.NewBorder(nil, nil, widget.NewLabel(letter),
+			container.NewHBox(widget.NewEntry(), container.NewGridWrap(fyne.Size{
+				Width:  10,
+				Height: 0,
+			})), nil))
+	}
+	alphabetVScroll := container.NewVScroll(alphabetVStack)
+
+	text := canvas.NewText("hello, world!", color.Black)
+	text.Alignment = fyne.TextAlignCenter
+
+	return container.NewBorder(title, nil, alphabetVScroll, nil, text)
+}
+
 func main() {
 	a := app.New()
 	w := a.NewWindow("Ciphers")
 
 	inputScreen := NewInputScreen(func() {
-		w.SetContent(container.NewWithoutLayout())
+		w.SetContent(NewMainScreen())
 	})
 	w.SetContent(inputScreen)
-	w.Resize(fyne.NewSize(500, 500))
+	w.Resize(fyne.NewSize(750, 500))
 	w.ShowAndRun()
 }
