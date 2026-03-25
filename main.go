bd2ea3a6bc7bac50aa02ec6f5ba74e53111aa256
Viriathus1 <49337323+Viriathus1@users.noreply.github.com>
2026-03-25 11:07:20 +1100

    added input screen

--- main.go
+++ main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
+func NewInputScreen(moveToMainScreen func()) *fyne.Container {
+	title := widget.NewLabel("Simple Substitution Cipher")
+
+	input := widget.NewEntry()
+	input.SetPlaceHolder("Enter ciphertext...")
+	input.MultiLine = true
+
+	saveButton := widget.NewButton("Save", moveToMainScreen)
+
+	return container.NewBorder(title, saveButton, nil, nil, input)
+}
+
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello")
+	w := a.NewWindow("Ciphers")
 
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
-
+	inputScreen := NewInputScreen(func() {
+		w.SetContent(container.NewWithoutLayout())
+	})
+	w.SetContent(inputScreen)
+	w.Resize(fyne.NewSize(500, 500))
 	w.ShowAndRun()
 }
